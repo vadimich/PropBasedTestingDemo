d919b3b5934a3f892b189505cfa7d62e971b38a1
Vadim Belobrovka <vadimich@gmail.com>
2016-02-27 11:59:29 -0500

    added provisioning for awesome vim

--- proj/test/propertytesting.js
+++ proj/test/propertytesting.js
@@ -5,11 +5,23 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var lib = process.env.JSCOV ? require('../lib-cov/propertytesting') : require('../lib/propertytesting');
+// var expect = require('chai').expect;
+var jsc = require('jsverify');
+// var lib = process.env.JSCOV ? require('../lib-cov/propertytesting') : require('../lib/propertytesting');
 
-describe('propertytesting module', function () {
-  it('internal exports object', function () {
-    expect(lib).to.be.an('object');
+describe('natural numbers', function () {
+  jsc.property('are greater or equal to zero', 'nat', function (n) {
+    return n >= 0;
+  });
+
+  jsc.property('added together result is larger than or equal to one of the nats', 'nat', 'nat', function (n1, n2) {
+    var sum = n1 + n2;
+    return sum >= n1 || sum >= n2;
+  });
+
+  jsc.property('also testing max function', 'number', 'number', function (n1, n2) {
+    var max = Math.max(n1, n2);
+    return max === n1 || max === n2;
   });
 });
+
